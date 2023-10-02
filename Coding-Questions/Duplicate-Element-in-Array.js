ed2c635ccdda9f9502b0d378115117e8afc11397
Harsh-Pandey <harshcse7571@gmail.com>
2023-10-02 07:06:49 -0700

    Solved

---
+++ Coding-Questions/Duplicate-Element-in-Array.js
@@ -0,0 +1,9 @@
+// We will be given a array and we have to find the duplicate element in the given array
+const arr = [1,2,3,2,8,7,8]
+const ans = arr.filter((item,index,array) =>{
+    return index != array.indexOf(item);
+})
+console.log(ans);
+
+
+// Index of always gives the first index at which the element occurse
